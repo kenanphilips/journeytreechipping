7737303e72bd51893c17bdbfd5432c6e81a263fc
Kenan Philips <philipskenan@gmail.com>
2017-10-28 13:23:50 -0700

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'https://dry-cove-85849.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'dry-cove-85849.herokuapp.com' }
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
@@ -47,10 +47,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'dry-cove-85849.herokuapp.com',
     user_name:            'journeygrad@gmail.com',
     password:             'Journey123',
-    authentication:       'plain',
+    authentication:       'login',
     # enable_starttls_auto: true
   }
 
