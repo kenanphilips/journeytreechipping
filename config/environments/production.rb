5a1096861dc148929b1471fdc61501f17a71f3a9
Kenan Philips <philipskenan@gmail.com>
2017-11-13 21:40:13 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
-  
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
