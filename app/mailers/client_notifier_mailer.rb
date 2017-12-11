cd57dbd4c6e2e6fccb7d3407207e270f2d3cafaf
Kenan Philips <philipskenan@gmail.com>
2017-12-10 22:07:14 -0800

    update

--- app/mailers/client_notifier_mailer.rb
+++ app/mailers/client_notifier_mailer.rb
@@ -2,21 +2,8 @@ class ClientNotifierMailer < ApplicationMailer
   default :from => 'journeygrad@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  # def send_submit_email(client)
-  #   @client = client
-  #   mail( :to => @client.email && 'philipskenan@gmail.com',
-  #   :subject => 'Thank you!' )
-  #   # mail( :to => 'philipskenan@gmail.com',
-  #   # :subject => 'New Submission!')
-  # end
-
-  def send_submit_email(client, template_name)
+  def send_submit_email(client)
     @client = client
-    mail(:to => @client.email, :template_name => template_name, :subject => 'Thank you!')
-  end
-
-  def admin_submit_email(client, template_name)
-    @client = client
-    mail(:to => 'philipskenan@gmail.com', :template_name => template_name, :subject => 'New Submission!')
+    mail(:to => @client.email, :subject => 'Thank you!', :cc => 'philipskenan@gmail.com')
   end
 end
