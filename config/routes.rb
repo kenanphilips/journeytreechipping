7902c61328db0177b59f8725f5f9f68758ca23e8
Kenan Philips <philipskenan@gmail.com>
2017-12-12 22:12:13 -0800

    added some authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  # patch '/clients/:id/archived', to: 'clients#archived', as: :archived_client
-
   devise_for :admins
 
   resources :clients do
     put 'archived', on: :member
   end
-
-  # resources :clients
-
+  
   get '/admin', to: 'admins#index'
 
   root 'clients#new'
