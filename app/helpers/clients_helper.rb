bb6efe7a7349ebcec539b38739e220b55c611c8f
Kenan Philips <philipskenan@gmail.com>
2018-01-05 17:12:52 -0800

    update

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,8 +2,8 @@ module ClientsHelper
 
   def options_for_pickup
     [
-      ['January 1st', "January 1st"],
-      ['January 6th', "January 6th"],
+      #['January 1st', "January 1st"],
+      #['January 6th', "January 6th"],
       ['January 7th', "January 7th"],
       ['January 13th', "January 13th"],
       ['January 14th', "January 14th"]
